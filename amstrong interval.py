608a637e2adf9463aaa6caa8108bf32951dfab36
kishore2904 <47620468+kishore2904@users.noreply.github.com>
2019-06-06 14:17:01 +0530

    Create amstrong interval.py

---
+++ amstrong interval.py
@@ -0,0 +1,12 @@
+num1,num2=input("").split()
+num1=int(num1)
+num2=int(num2)
+for i in range(num1,num2):
+     s=0
+     a=i
+     while(i!=0):
+          rem=i%10
+          s=s+rem*rem*rem
+          i//=10
+     if(a==s):
+        print(a,end" ")
