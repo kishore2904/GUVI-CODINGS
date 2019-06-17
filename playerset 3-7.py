cde7af823e34e84ff09aed60c179c57f96265ac1
kishore2904 <47620468+kishore2904@users.noreply.github.com>
2019-06-17 10:17:10 +0530

    Create playerset 3-7.py

---
+++ playerset 3-7.py
@@ -0,0 +1,8 @@
+    
+sa=list(map(str,input()))
+for i in range(len(sa)):
+  if sa[i].islower()==True:
+        sa[i]=s[i].capitalize()
+  else:
+        sai]=s[i].lower()
+for i in range(len(sa)): print(sa[i],end="")
