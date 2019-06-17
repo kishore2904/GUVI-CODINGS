29ece7a44e191de262deb10dd802d0676bb4081d
kishore2904 <47620468+kishore2904@users.noreply.github.com>
2019-06-17 10:19:08 +0530

    Update playerset 3-7.py

--- playerset 3-7.py
+++ playerset 3-7.py
@@ -4,5 +4,5 @@ for i in range(len(sa)):
   if sa[i].islower()==True:
         sa[i]=s[i].capitalize()
   else:
-        sai]=s[i].lower()
+        sa[i]=sa[i].lower()
 for i in range(len(sa)): print(sa[i],end="")
